43a03f02d6ff62db63e92e750b6495815444dce2
ViLLXM <av2096s32@gmail.com>
2025-10-04 20:58:24 +0300

    Delete mistakes in comments

--- rectangle.py
+++ rectangle.py
@@ -1,32 +1,14 @@
 def area(a, b):
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 3b66537 (Added comments to functions)
     '''
     Принимает числа a, b возвращает площадь прямоугольника со сторонами a, b
     Например, для чисел 4 и 5 будет возвращено число 20
     '''
-<<<<<<< HEAD
-=======
->>>>>>> 643ae41 (Rectangle.py and and triangle.py was added)
-=======
->>>>>>> 3b66537 (Added comments to functions)
     return a * b
 
 
 def perimeter(a, b):
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 3b66537 (Added comments to functions)
     '''
     Принимает числа a, b, возвращает периметр прямоугольника со сторонами a, b
     Например, для чисел 4 и 5 будет возвращено число 18
     '''
-<<<<<<< HEAD
-=======
->>>>>>> 643ae41 (Rectangle.py and and triangle.py was added)
-=======
->>>>>>> 3b66537 (Added comments to functions)
     return (a + b) * 2
--- triangle.py
+++ triangle.py
@@ -1,32 +1,13 @@
 def area(a, h):
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 3b66537 (Added comments to functions)
     '''
     Принимает числа a, h возвращает площадь треугольника со стороной a и высотой h
     Например, для чисел 5, 6 будет возвращено число 15
     '''
-<<<<<<< HEAD
-    return a * h / 2 
+    return a * h / 2
 
 def perimeter(a, b, c):
     '''
     Принимает числа a, b, c возвращает периметр треугольника со сторонами a, b, c
     Например, для чисел 5, 6, 7 будет возвращено число 18
     '''
-=======
-    return a * h / 2 
-
-def perimeter(a, b, c): 
->>>>>>> 643ae41 (Rectangle.py and and triangle.py was added)
-=======
-    return a * h / 2 
-
-def perimeter(a, b, c):
-    '''
-    Принимает числа a, b, c возвращает периметр треугольника со сторонами a, b, c
-    Например, для чисел 5, 6, 7 будет возвращено число 18
-    '''
->>>>>>> 3b66537 (Added comments to functions)
     return a + b + c 
